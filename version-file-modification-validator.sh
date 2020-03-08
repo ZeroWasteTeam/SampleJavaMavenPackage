d01e6c58859bf449c28f98b3b1714aa1b22e7626
Nachiappan <nachiapan@gmail.com>
2020-03-08 19:37:57 +0530

    Done

--- version-file-modification-validator.sh
+++ version-file-modification-validator.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-ref1=$1
-ref2=$2
+function validateVersionFileModification {
 
-echo "Checking Version File is Modified"
-numberOfFilesModified=$(git diff --name-only "$ref1..$ref2" | wc -l)
-isVersionFileModified=$(git diff --name-only "$ref1..$ref2" version.txt | wc -l)
+  ref1=$1
+  ref2=$2
 
-initialVersion=$(git show $ref1:version.txt)
-finalVersion=$(git show $ref2:version.txt)
-echo $initialVersion
-echo $finalVersion
-
-if [ $isVersionFileModified  -eq 1 ]
-then
-  if [ $numberOfFilesModified -gt 1 ]
+  echo "Checking Version File is Modified"
+  numberOfFilesModified=$(git diff --name-only "$ref1..$ref2" | wc -l)
+  isVersionFileModified=$(git diff --name-only "$ref1..$ref2" version.txt | wc -l)
+  
+  initialVersion=$(git show $ref1:version.txt)
+  finalVersion=$(git show $ref2:version.txt)
+  
+  if [ $isVersionFileModified  -eq 1 ]
   then
-    echo 'Version file modified along with other files. To Change version, no other file should be modified'
-	exit 1;
+    if [ $numberOfFilesModified -gt 1 ]
+    then
+  	  echo 'Version file modified along with other files. To Change version, no other file should be modified'
+  	  exit 1;
+    fi
+    if (( $(echo "$initialVersion > $finalVersion" |bc -l) ))
+    then
+  	  echo 'Version number is not properly incremented'
+  	  exit 1;
+    fi
   fi
-  if (( $(echo "$initialVersion > $finalVersion" |bc -l) ))
-  then
-	echo 'Version number is not properly incremented'
-	exit 1;
-  fi
-fi
+}
+
+validateVersionFileModification $1 $2
