56eb0e8bf5d77a3f182e432afd382413246d31d4
Nachiappan <nachiapan@gmail.com>
2020-03-08 19:28:56 +0530

    no message

--- version-file-modification-validator.sh
+++ version-file-modification-validator.sh
@@ -7,18 +7,18 @@ echo "Checking Version File is Modified"
 numberOfFilesModified=$(git diff --name-only "$ref1..$ref2" | wc -l)
 isVersionFileModified=$(git diff --name-only "$ref1..$ref2" version.txt | wc -l)
 
-
 initialVersion=$(git show $ref1:version.txt)
 finalVersion=$(git show $ref2:version.txt)
 echo $initialVersion
 echo $finalVersion
 
-
-[ $numberOfFilesModified -gt 1 ] && [ $isVersionFileModified  -eq 1 ] &&  { echo 'Version file modified along with other files. To Change version, no other file should be modified' ; exit 1; }
-
-
 if [ $isVersionFileModified  -eq 1 ]
 then
+  if [ $numberOfFilesModified -gt 1 ]
+  then
+    echo 'Version file modified along with other files. To Change version, no other file should be modified'
+	exit 1;
+  fi
   if (( $(echo "$initialVersion > $finalVersion" |bc -l) ))
   then
 	echo 'Version number is not properly incremented'
