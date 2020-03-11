7918eeee066f2a0badf1e8a8a1aa9e27a6095668
Nachiappan <nachiapan@gmail.com>
2020-03-11 11:59:05 +0530

    Version utility corrected

--- version-utility.sh
+++ version-utility.sh
@@ -37,7 +37,7 @@ validateBaseVersion() {
   fi
 }
 
-getCheckoutLatestCommitSha() {
+getCheckoutedOutLatestCommitSha() {
   local GIT_SHA=$(git log -n1 --format=format:"%H")
   if [[ "${#GIT_SHA}" -lt 8 ]]
   then
@@ -57,10 +57,79 @@ isCommitInReferenceBranch() {
   return 0
 }
 
-#BASE_VERSION=$(readBaseVersion)
-#validateBaseVersion ${BASE_VERSION}
-GIT_SHA=$(getCheckoutLatestCommitSha)
-isCommitInReferenceBranch ${GIT_SHA}
+getGitShaForLatestVersionChange() {
+  local GIT_SHA=$(git log  -n 1 --format=format:%H ${VERSION_FILE})
+  if [[ "${#GIT_SHA}" -lt 8 ]]
+  then
+    echo "Could not get Git Sha. Ensure you are running the script in a git repo with "${VERSION_FILE}" file" >&2
+	return 1
+  fi
+  echo ${GIT_SHA}
+}
+
+getNumberOfCommitsSinceVersionChange() {
+  local VERSION_CHANGE_COMMIT_SHA=$(getGitShaForLatestVersionChange)
+  if [[ "${?}" = 0 ]]
+  then
+	return 1
+  fi
+  NUMBER_OF_COMMITS=$(git rev-list ${VERSION_CHANGE_COMMIT_SHA}..HEAD --count)
+  
+  
+}
+
+getVersion() {
+  local BASE_VERSION=$(readBaseVersion)
+  echo "CHECK ${?}"
+  if ! [[ "${?}" == 0 ]]
+  then
+	echo "Could not read base version from ${VERSION_FILE}" >&2
+	return 1
+  fi
+  
+  if ! [[ $(validateBaseVersion $BASE_VERSION) ]]
+  then
+	echo "In Valid Base Version" >&2
+	return 1
+  fi
+  
+  local GIT_SHORT_SHA=$(git rev-parse --short=7 $(git log -n1 --format=format:%H))
+  if [[ "${#GIT_SHORT_SHA}" -lt 8 ]]
+  then
+    echo "Could not get Git Sha" >&2
+	return 1
+  fi
+  
+  
+  if [[ $(isCommitInReferenceBranch $GIT_SHA) ]]
+  then
+    local BUILD_NUMBER=$(git rev-list $(getGitShaForLatestVersionChange)..HEAD --count)
+	if ! [[ "${?}" == 0 ]]
+	then
+	  echo "Could not read build number" >&2
+	  return 1;
+	fi
+	
+	local VERSION="${BASE_VERSION}.${BUILD_NUMBER}"
+    if [ "${BUILD_NUMBER}" != 0 ]
+    then
+      VERSION="${VERSION}-${GIT_SHORT_SHA}"
+    fi
+	
+  else  
+    local DATE_VERSION=$(git log -1 --format="%at" | xargs -I{} date -d @{} +%Y-%m-%d-%H-%M-%S)
+	if ! [[ "${?}" == 0 ]]
+	then
+	  echo "Could not read date version" >&2
+	  return 1
+	fi
+	
+    local VERSION="${BASE_VERSION}-${DATE_VERSION}-${GIT_SHORT_SHA}"
+  fi
+  echo $VERSION
+}
+
+getVersion
 echo "return value ${?}"  
 
 
