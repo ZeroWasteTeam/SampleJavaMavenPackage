2d751bce65a2c9e7c47254219c8e7a261d021a2b
Nachiappan <nachiapan@gmail.com>
2020-03-14 20:42:59 +0530

    Assert commit in branch checked in

--- assertCommitInBranch.sh
+++ assertCommitInBranch.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
+set -e
+
 COMMIT=$1
 BRANCH=$2
 
-TEMP=$(git log ${BRANCH} --format=format:%H | grep ${COMMIT} | wc -l)
-
-if [[ "${?}" != 0 ]] ; 
-then 
-  echo >&2 "Unable to perform operation. Ensure it's a git repo and installation of git"
-  exit 1;
-fi
-
-if [[ "${TEMP}" = 0 ]] ;
-then
-  echo >&2 "commit not in branch"
-  exit 1;
-fi
+test $(git log ${BRANCH} --format=format:%H | grep ${COMMIT} | wc -l) -eq 1 || exit 1
 
